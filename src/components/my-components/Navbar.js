ff46a683b94f4333662a757abb4e6dc9238fde67
Alex Wurm <aewurm98@gmail.com>
2022-06-10 20:52:04 -0700

    added animatescroll functionality

--- src/components/my-components/Navbar.js
+++ src/components/my-components/Navbar.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link as LinkR } from "react-router-dom";
+import { Link as LinkS, animateScroll as scroll} from "react-scroll";
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import MKButton from "components/MKButton";
-
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -26,63 +26,66 @@ function Navbar() {
     showButton();
   }, []);
 
-  window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
+
+  const toTop = () => {
+    scroll.scrollToTop();
+
+  const toggleHome = () => {
+    {closeMobileMenu}
+    scroll.scrollToTop();
+  };
 
   return (
     <>
-      <nav className='navbar'>
-
-        <div className='navbar-container'>
-
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <LinkR to="/" className="navbar-logo" onClick={toggleHome}>
             <i class="fa-solid fa-basketball"></i>
             Alex Wurm
-          </Link>
+          </LinkR>
 
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
 
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
-            <li className='nav-item'>
-              <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <LinkR to="/about" className="nav-links" onClick={closeMobileMenu}>
                 About
-              </Link>
+              </LinkR>
             </li>
 
-            <li className='nav-item'>
-              <Link
-                to='/work-experience'
-                className='nav-links'
+            <li className="nav-item">
+              <LinkR
+                to="/work-experience"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
                 Work Experience
-              </Link>
+              </LinkR>
             </li>
 
-            <li className='nav-item'>
-              <Link
-                to='/projects'
-                className='nav-links'
+            <li className="nav-item">
+              <LinkR
+                to="/projects"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
                 Projects
-              </Link>
+              </LinkR>
             </li>
 
             <li>
-              <Link
-                to='/learn-more'
-                className='nav-links-mobile'
+              <LinkR
+                to="/learn-more"
+                className="nav-links-mobile"
                 onClick={closeMobileMenu}
               >
                 Learn More
-              </Link>
+              </LinkR>
             </li>
-
           </ul>
-
         </div>
       </nav>
     </>
