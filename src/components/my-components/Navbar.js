40aa90b92b4a215b06208da685d4f3a33837458f
Alex Wurm <aewurm98@gmail.com>
2022-06-11 17:21:32 -0700

    added outslide click handler

--- src/components/my-components/Navbar.js
+++ src/components/my-components/Navbar.js
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 // react-router components
 import { Link } from "react-router-dom";
 import { Link as LinkS } from "react-scroll";
+
+// other react library components
+import OutsideClickHandler from "react-outside-click-handler";
 
 // prop-types is a library for typechecking of props.
 import PropTypes from "prop-types";
@@ -29,16 +32,7 @@ const navItemLogo3 = <i class="fa-solid fa-code"></i>;
 const navItemLogo4 = <i class="fa-solid fa-circle-info"></i>;
 
 //TODO: Investigate porential to increase span size to make clicking easier
-function Navbar({
-  logo,
-  brand,
-  transparent,
-  light,
-  sticky,
-  relative,
-  center,
-}) {
-
+function Navbar({ logo, brand, transparent, light, sticky, relative, center }) {
   const [mobileNavbar, setMobileNavbar] = useState(false);
   const [mobileView, setMobileView] = useState(false);
 
@@ -90,65 +84,49 @@ function Navbar({
   // });
 
   return (
-    
-    //Overall Navbar
-    <Container sx={sticky ? { position: "sticky", top: 0, zIndex: 10 } : null}>
-
-      {/* Core Navbar element */}
-      <MKBox
-        py={1}
-        px={{ xs: 4, sm: transparent ? 2 : 3, lg: transparent ? 0 : 2 }}
-        my={relative ? 0 : 2}
-        mx={relative ? 0 : 3}
-        width={relative ? "100%" : "calc(100% - 48px)"}
-        borderRadius="xl"
-        shadow={transparent ? "none" : "md"}
-        color={light ? "white" : "dark"}
-        position={relative ? "relative" : "absolute"}
-        left={0}
-        zIndex={3}
-        sx={({
-          palette: { transparent: transparentColor, white },
-          functions: { rgba },
-        }) => ({
-          backgroundColor: transparent
-            ? transparentColor.main
-            : rgba(white.main, 0.8),
-          backdropFilter: transparent ? "none" : `saturate(200%) blur(30px)`,
-        })}
+    <OutsideClickHandler onOutsideClick={() => setMobileNavbar(false)}>
+      //Overall Navbar
+      <Container
+        sx={sticky ? { position: "sticky", top: 0, zIndex: 10 } : null}
       >
-        {/* Navbar items */}
-        <MKBox display="flex" justifyContent="start" alignItems="center">
-          <MKBox
-            to="/"
-            lineHeight={1}
-            py={transparent ? 1.5 : 0.75}
-            pl={relative || transparent ? 0 : { xs: 0, lg: 1 }}
-            display="flex"
-            flex={1}
-            justifyContent="start"
-            alignItems="center"
-            // Added Navbar close on Link click
-            onClick={closeMobileNavbar}
-          >
-            <MKBox mr={1} component={Link} to="home">
-              <LinkS
-                to="home"
-                smooth={true}
-                duration={500}
-                spy={true}
-                exact={true}
-                offset={0}
-              >
-                {siteLogo}
-              </LinkS>
-            </MKBox>
-            <MKBox component={Link} to="home">
-              <MKTypography
-                variant="button"
-                fontWeight="bold"
-                color={light ? "white" : "dark"}
-              >
+        {/* Core Navbar element */}
+        <MKBox
+          py={1}
+          px={{ xs: 4, sm: transparent ? 2 : 3, lg: transparent ? 0 : 2 }}
+          my={relative ? 0 : 2}
+          mx={relative ? 0 : 3}
+          width={relative ? "100%" : "calc(100% - 48px)"}
+          borderRadius="xl"
+          shadow={transparent ? "none" : "md"}
+          color={light ? "white" : "dark"}
+          position={relative ? "relative" : "absolute"}
+          left={0}
+          zIndex={3}
+          sx={({
+            palette: { transparent: transparentColor, white },
+            functions: { rgba },
+          }) => ({
+            backgroundColor: transparent
+              ? transparentColor.main
+              : rgba(white.main, 0.8),
+            backdropFilter: transparent ? "none" : `saturate(200%) blur(30px)`,
+          })}
+        >
+          {/* Navbar items */}
+          <MKBox display="flex" justifyContent="start" alignItems="center">
+            <MKBox
+              to="/"
+              lineHeight={1}
+              py={transparent ? 1.5 : 0.75}
+              pl={relative || transparent ? 0 : { xs: 0, lg: 1 }}
+              display="flex"
+              flex={1}
+              justifyContent="start"
+              alignItems="center"
+              // Added Navbar close on Link click
+              onClick={closeMobileNavbar}
+            >
+              <MKBox mr={1} component={Link} to="home">
                 <LinkS
                   to="home"
                   smooth={true}
@@ -157,141 +135,158 @@ function Navbar({
                   exact={true}
                   offset={0}
                 >
-                  {brand}
+                  {siteLogo}
                 </LinkS>
-              </MKTypography>
+              </MKBox>
+              <MKBox component={Link} to="home">
+                <MKTypography
+                  variant="button"
+                  fontWeight="bold"
+                  color={light ? "white" : "dark"}
+                >
+                  <LinkS
+                    to="home"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact={true}
+                    offset={0}
+                  >
+                    {brand}
+                  </LinkS>
+                </MKTypography>
+              </MKBox>
             </MKBox>
-          </MKBox>
-          <MKBox
-            color="inherit"
-            display={{ xs: "none", lg: "flex" }}
-            flex={{ xs: 0, lg: 1 }}
-            ml="auto"
-            mr={center ? "auto" : 0}
-          >
             <MKBox
-              mx={0}
-              p={0}
-              width="100%"
-              display="flex"
-              direction="column"
-              justifyContent="center"
-              alignItems="flex-end"
-              color={light ? "white" : "dark"}
-              opacity={light ? 1 : 0.6}
-              sx={{ cursor: "pointer", userSelect: "none" }}
+              color="inherit"
+              display={{ xs: "none", lg: "flex" }}
+              flex={{ xs: 0, lg: 1 }}
+              ml="auto"
+              mr={center ? "auto" : 0}
             >
-              <MKTypography
-                variant="button"
-                fontWeight="medium"
+              <MKBox
+                mx={0}
+                p={0}
+                width="100%"
+                display="flex"
+                direction="column"
+                justifyContent="center"
+                alignItems="flex-end"
                 color={light ? "white" : "dark"}
-                mx="auto"
+                opacity={light ? 1 : 0.6}
+                sx={{ cursor: "pointer", userSelect: "none" }}
               >
-                <LinkS
-                  to="about"
-                  smooth={true}
-                  duration={500}
-                  spy={true}
-                  exact={true}
-                  offset={0}
-                >
-                  About
-                </LinkS>
-              </MKTypography>
-              <MKTypography
-                variant="button"
-                fontWeight="medium"
-                color={light ? "white" : "dark"}
-                mx="auto"
-              >
-                <LinkS
-                  to="work-experience"
-                  smooth={true}
-                  duration={500}
-                  spy={true}
-                  exact={true}
-                  offset={0}
-                >
-                  Work Experience
-                </LinkS>
-              </MKTypography>
-              <MKTypography
-                variant="button"
-                fontWeight="medium"
-                color={light ? "white" : "dark"}
-                mx="auto"
-              >
-                <LinkS
-                  to="projects"
-                  smooth={true}
-                  duration={500}
-                  spy={true}
-                  exact={true}
-                  offset={0}
-                >
-                  Projects
-                </LinkS>
-              </MKTypography>
-              <MKTypography
-                variant="button"
-                fontWeight="medium"
-                color={light ? "white" : "dark"}
-                mx="auto"
-              >
-                <LinkS
-                  to="learn-more"
-                  smooth={true}
-                  duration={500}
-                  spy={true}
-                  exact={true}
-                  offset={0}
-                >
-                  Learn More
-                </LinkS>
-              </MKTypography>
-            </MKBox>
-          </MKBox>
-          <MKBox
-            display={{ xs: "grid", lg: "none" }}
-            lineHeight={0}
-            justifyContent="center"
-            py={1.5}
-            pl={1.5}
-            color={transparent ? "white" : "inherit"}
-            sx={{ cursor: "pointer" }}
-            onClick={openMobileNavbar}
-          >
-            <Icon fontSize="default">{mobileNavbar ? "close" : "menu"}</Icon>
-          </MKBox>
-        </MKBox>
-        <MKBox
-          bgColor={transparent ? "white" : "transparent"}
-          shadow={transparent ? "lg" : "none"}
-          borderRadius="xl"
-          px={transparent ? 2 : 0}
-        >
-          {/* Mobile Dropdown Menu */}
-          {mobileView && mobileNavbar && (
-            <MKBox
-              mt={2}
-              mx={0}
-              p={0}
-              minHeight="20vh"
-              display="flex"
-              direction="row"
-              justifyContent="start"
-              alignItems="start"
-              color={light ? "white" : "dark"}
-              opacity={light ? 1 : 0.6}
-              sx={{ cursor: "pointer", userSelect: "none" }}
-            >
-              <Stack mr={3} spacing={2}>
                 <MKTypography
                   variant="button"
                   fontWeight="medium"
                   color={light ? "white" : "dark"}
-                  my="auto"
+                  mx="auto"
                 >
+                  <LinkS
+                    to="about"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact={true}
+                    offset={0}
+                  >
+                    About
+                  </LinkS>
+                </MKTypography>
+                <MKTypography
+                  variant="button"
+                  fontWeight="medium"
+                  color={light ? "white" : "dark"}
+                  mx="auto"
+                >
+                  <LinkS
+                    to="work-experience"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact={true}
+                    offset={0}
+                  >
+                    Work Experience
+                  </LinkS>
+                </MKTypography>
+                <MKTypography
+                  variant="button"
+                  fontWeight="medium"
+                  color={light ? "white" : "dark"}
+                  mx="auto"
+                >
+                  <LinkS
+                    to="projects"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact={true}
+                    offset={0}
+                  >
+                    Projects
+                  </LinkS>
+                </MKTypography>
+                <MKTypography
+                  variant="button"
+                  fontWeight="medium"
+                  color={light ? "white" : "dark"}
+                  mx="auto"
+                >
+                  <LinkS
+                    to="learn-more"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact={true}
+                    offset={0}
+                  >
+                    Learn More
+                  </LinkS>
+                </MKTypography>
+              </MKBox>
+            </MKBox>
+            <MKBox
+              display={{ xs: "grid", lg: "none" }}
+              lineHeight={0}
+              justifyContent="center"
+              py={1.5}
+              pl={1.5}
+              color={transparent ? "white" : "inherit"}
+              sx={{ cursor: "pointer" }}
+              onClick={openMobileNavbar}
+            >
+              <Icon fontSize="default">{mobileNavbar ? "close" : "menu"}</Icon>
+            </MKBox>
+          </MKBox>
+          <MKBox
+            bgColor={transparent ? "white" : "transparent"}
+            shadow={transparent ? "lg" : "none"}
+            borderRadius="xl"
+            px={transparent ? 2 : 0}
+          >
+            {/* Mobile Dropdown Menu */}
+            {mobileView && mobileNavbar && (
+              <MKBox
+                mt={2}
+                mx={0}
+                p={0}
+                minHeight="20vh"
+                display="flex"
+                direction="row"
+                justifyContent="start"
+                alignItems="start"
+                color={light ? "white" : "dark"}
+                opacity={light ? 1 : 0.6}
+                sx={{ cursor: "pointer", userSelect: "none" }}
+              >
+                <Stack mr={3} spacing={2}>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="about"
                       smooth={true}
@@ -302,13 +297,13 @@ function Navbar({
                     >
                       {navItemLogo1}
                     </LinkS>
-                </MKTypography>
-                <MKTypography
-                  variant="button"
-                  fontWeight="medium"
-                  color={light ? "white" : "dark"}
-                  my="auto"
-                >
+                  </MKTypography>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="work-experience"
                       smooth={true}
@@ -319,13 +314,13 @@ function Navbar({
                     >
                       {navItemLogo2}
                     </LinkS>
-                </MKTypography>
-                <MKTypography
-                  variant="button"
-                  fontWeight="medium"
-                  color={light ? "white" : "dark"}
-                  my="auto"
-                >
+                  </MKTypography>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="projects"
                       smooth={true}
@@ -336,13 +331,13 @@ function Navbar({
                     >
                       {navItemLogo3}
                     </LinkS>
-                </MKTypography>
-                <MKTypography
-                  variant="button"
-                  fontWeight="medium"
-                  color={light ? "white" : "dark"}
-                  my="auto"
-                >
+                  </MKTypography>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="learn-more"
                       smooth={true}
@@ -353,15 +348,15 @@ function Navbar({
                     >
                       {navItemLogo4}
                     </LinkS>
-                </MKTypography>
-              </Stack>
-              <Stack spacing={2}>
-                <MKTypography
-                  variant="button"
-                  fontWeight="medium"
-                  color={light ? "white" : "dark"}
-                  my="auto"
-                >
+                  </MKTypography>
+                </Stack>
+                <Stack spacing={2}>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="about"
                       smooth={true}
@@ -372,13 +367,13 @@ function Navbar({
                     >
                       About
                     </LinkS>
-                </MKTypography>
-                <MKTypography
-                  variant="button"
-                  fontWeight="medium"
-                  color={light ? "white" : "dark"}
-                  my="auto"
-                >
+                  </MKTypography>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="work-experience"
                       smooth={true}
@@ -389,13 +384,13 @@ function Navbar({
                     >
                       Work Experience
                     </LinkS>
-                </MKTypography>
-                <MKTypography
-                  variant="button"
-                  fontWeight="medium"
-                  color={light ? "white" : "dark"}
-                  my="auto"
-                >
+                  </MKTypography>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="projects"
                       smooth={true}
@@ -406,13 +401,13 @@ function Navbar({
                     >
                       Projects
                     </LinkS>
-                </MKTypography>
-                <MKTypography
-                  variant="button"
-                  fontWeight="medium"
-                  color={light ? "white" : "dark"}
-                  my="auto"
-                >
+                  </MKTypography>
+                  <MKTypography
+                    variant="button"
+                    fontWeight="medium"
+                    color={light ? "white" : "dark"}
+                    my="auto"
+                  >
                     <LinkS
                       to="learn-more"
                       smooth={true}
@@ -423,13 +418,14 @@ function Navbar({
                     >
                       Learn More
                     </LinkS>
-                </MKTypography>
-              </Stack>
-            </MKBox>
-          )}
+                  </MKTypography>
+                </Stack>
+              </MKBox>
+            )}
+          </MKBox>
         </MKBox>
-      </MKBox>
-    </Container>
+      </Container>
+    </OutsideClickHandler>
   );
 }
 
